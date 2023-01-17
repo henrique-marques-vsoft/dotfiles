b47690b419a8cf089f841e2f9c259b87009a5f93
hubconfeccao <allyson2308h@gmail.com>
2023-01-17 06:51:13 -0300

    add files

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#use yes | <command> to execute without confirmation
+#--disable-download-timeout to execute withou timetout
+yes | pacman -S firefox gimp yay i3status neofetch gnome-terminal rofi picom nitrogen feh make vtop dmenu ranger nnn flameshot git github-desktop-bin mpv psensor nodejs qbittorrent nautilus arandr
+
+#etc.
